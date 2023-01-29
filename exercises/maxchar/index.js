1ba1237766d3bfd9e4f6dafa6e01357c01378910
therobq <robert.quan@dts-global.com>
2023-01-29 16:09:05 -0700

    passed all tests

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const charMap = {};
+    let char = '';
+    let counter = 0;
+    for(let i = 0; i < str.length; i++) {
+        if(charMap[str[i]]) {
+            charMap[str[i]]++
+        } else {
+            charMap[str[i]] = 1
+        };
+    };
+
+    for(let key in charMap) {
+        if(charMap[key] > counter) {
+            counter = charMap[key];
+            char = key
+        }
+    };
+
+    return char;
+
+}
 
 module.exports = maxChar;
